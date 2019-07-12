5ec9e91385fc874fed306ec3a60a52369c1af3dc
Smestern <SMestern@uwo.ca>
2019-07-12 13:10:29 -0400

    dataX

--- data club 197030001/ABFplayback.py
+++ data club 197030001/ABFplayback.py
@@ -7,30 +7,28 @@ import time
 import tkinter as tk
 from tkinter import filedialog
 
+
+#Opens file open dialog
 root = tk.Tk()
 root.withdraw()
-
 file_path = filedialog.askopenfilename()
+abf = pyabf.ABF(file_path) #If you would prefer you can change file_path to manually point at a specific file
 
-
-abf = pyabf.ABF(file_path)
-#plt.figure(figsize=(10, 8))
+#turn on interactive mode
 plt.ion()
 
-cm = plt.get_cmap("Set1")
+
+#### EASY SETTINGS ######
+cm = plt.get_cmap("Set1") #Changes colour based on sweep number
 colors = [cm(x/abf.sweepCount * 1.25) for x in abf.sweepList]
-#plt.gca().axis('on') # hide axes to enhance floating effect
-#plt.autoscale(False)
-#plt.xlim(0, 3)
-#plt.ylim(-100, 50)
+
 
 sweepNumber=4
 
 abf.setSweep(sweepNumber)
 
-i1, i2 = 0, 1 # plot part of the sweep
-dataX = abf.sweepX[i1:i2] # + .25 * sweepNumber
-dataY = abf.sweepY[i1:i2]  # + 100 * sweepNumber
+i1, i2 = 0, 1 #defines the number of points
+
 
 framestodisplay = int((abf.dataPointsPerMs * 3000) / 100)
 print(abf.dataPointsPerMs)
@@ -43,9 +41,11 @@ plt.pause(1)
 plt.ylabel(abf.sweepLabelY)
 plt.xlabel(abf.sweepLabelX)
 
+
 def init():
     ax.set_xlim(0, 3)
     ax.set_ylim(-100, 50)
+    plt.figure(figsize=(10, 8))
     return ln,
 
 def update(i):
@@ -90,22 +90,6 @@ plt.show()
 #writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 #ani.save("movie.mp4")
 plt.pause(1000)
-#for sweepNumber in abf.sweepList:
-#        plt.Figure.clear
-#        if (sweepNumber <= 7 & sweepNumber > 1):
- #           abf.setSweep(sweepNumber + 1)
-  #          j = 0
-   #         k = abf.sweepPointCount
-    #        while j <= k:
-     #               plt.pause(0.001)
-      #              print(j)
-       #             print("of")
-        #            print(k)
-         #           i1, i2 = 0, j # plot part of the sweep
-          #          dataX = abf.sweepX[i1:i2] # + .25 * sweepNumber
-           #         dataY = abf.sweepY[i1:i2]  # + 100 * sweepNumber
-            #        plt.plot(dataX, dataY, color=colors[sweepNumber], alpha=.5)
-             #       j += int(k * 0.10)
 
 
 
